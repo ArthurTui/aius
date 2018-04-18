cb59fa6f720d58d9372bd50dca7666ebb04903a3
henrique cerquinho <henriquecerquinho@gmail.com>
2018-04-18 13:54:12 -0300

    alteraçoes na tela de pause nao tinham sido commitadas, aparentemente

--- maguinhos/stages/pause_menu.gd
+++ maguinhos/stages/pause_menu.gd
@@ -7,12 +7,13 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
-		_on_unpause_pressed()
+		_on_resume_pressed()
 
 
-func _on_unpause_pressed():
+func _on_resume_pressed():
 	get_tree().paused = not get_tree().is_paused()
 	self.visible = not self.is_visible()
+	
 
 func _on_quit_pressed():
 	get_tree().change_scene("res://menus/main_menu.tscn")
