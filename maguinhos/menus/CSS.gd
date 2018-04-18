7c39d3a078118bf5e7e71601ff28dfb24373406d
henrique cerquinho <henriquecerquinho@gmail.com>
2018-04-18 16:06:53 -0300

    Correções no comportamento da CSS transição da CSS para batalha em progresso

--- maguinhos/menus/CSS.gd
+++ maguinhos/menus/CSS.gd
@@ -3,30 +3,39 @@ extends Control
 const characters = ["skeleton", "broleton", "sealeton", "bloodyskel"]
 
 var active_devices = {} # which device controls which player
-var selected_characters = [0,0,0,0]
-var available_characters = [0,1,2,3]
+var selected_characters = [null,null,null,null]
+var available_characters = ["skeleton", "broleton", "sealeton", "bloodyskel"]
 
 var active_players = 0 # number of active players
 var ready_players = [] # which players are active
 
-var player
 
 func _ready():
+	$battle.hide()
 	set_process_input(true)
 
+
 func _input(event):
+	var player
 	var id = event.device
+	
+	
 	# player enters the game
 	if Input.is_action_just_pressed("pause"):
 		if not id in active_devices and active_players < 4:
-			active_players += 1
-			active_devices[id] = active_players
-			player = active_devices[id]
-			selected_characters[player - 1] = available_characters.pop_front()
-			get_node(str("P", active_players,"/anim")).play("enter")
-			
-			var character = characters[selected_characters[player - 1]]
-			get_node(str("P", active_players,"/character")).set_animation(character)
+			for pl in range(4):
+				# searches for the first empty slot in the CSS
+				# and adds the player there
+				if selected_characters[pl] == null:
+					active_players += 1
+					selected_characters[pl] = available_characters.pop_front()
+					active_devices[id] = pl + 1
+					
+					var character = selected_characters[pl]
+					get_node(str("P", pl + 1,"/anim")).play("enter")
+					print("player",pl)
+					get_node(str("P", active_players,"/character")).set_animation(character)
+					break
 		
 		else:
 			player = active_devices[id]
@@ -40,15 +49,19 @@ func _input(event):
 				ready_players.remove(ready_players.find(player))
 			print(ready_players)
 		
+		
 		# starts game
 		if ready_players.size() == active_players and active_players >= 2:
-			print("lesgo bois")
+			$battle.start(active_devices, selected_characters)
+			$battle.show()
+			print("lesgo")
 			pass
 		print(active_devices)
 	
+	
 	# player changes character
 	elif Input.is_action_just_pressed("ui_right") or Input.is_action_just_pressed("ui_left"):
-		if active_players > 0:
+		if active_players > 0 and id in active_devices:
 			player = active_devices[id]
 			
 			# ready players cannot change character
@@ -62,25 +75,20 @@ func _input(event):
 					available_characters.push_front(selected_characters[player - 1])
 					selected_characters[player - 1] = available_characters.pop_back()
 				
-				var character = characters[selected_characters[player - 1]]
+				var character = selected_characters[player - 1]
 				get_node(str("P", player, "/character")).set_animation(character)
+		print(selected_characters)
+	
 	
 	# player "exits"
 	elif Input.is_action_just_pressed("ui_cancel"):
 		if id in active_devices and not active_devices[id] in ready_players:
-			get_node(str("P", active_players,"/anim")).play("exit")
-			
 			player = active_devices[id]
+			
+			get_node(str("P", player,"/anim")).play("exit")
+			
 			available_characters.push_front(selected_characters[player - 1])
-			selected_characters[player - 1] = -1
+			selected_characters[player - 1] = null
 			active_players -= 1
 			active_devices.erase(id)
-			
-# MOST ARRAYS STILL UNTESTED/PRINTED
-
-
-
-
-
-
 
---
+++ maguinhos/stages/battle.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var positions = [Vector2(200, 300), Vector2(800, 300), Vector2(510, 100), Vector2(510, 500)]
+
+var char_scn = preload("res://characters/character.tscn")
+
+var living = []
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func start(active_players, selected_characters):
+	# which ports are living
+	for key in active_players.keys():
+		living.append(key)
+		
+		var char_inst = char_scn.insance()
+		char_inst.set_name(str("Character", active_players[key]))
+		char_inst.position = positions[active_players[key] - 1]
+		
+		char_inst.get_node("sprite").set_animation(selected_characters[active_players[key] - 1])
+		
+		add_child(char_inst)
+	set_process(true)
