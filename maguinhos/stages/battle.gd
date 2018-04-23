57c51b46e9da454f033ea02f4bddd2c401996a4e
cerco <henriquecerquinho@gmail.com>
2018-04-23 10:56:08 -0300

    condição de vitória adicionada

--- maguinhos/stages/battle.gd
+++ maguinhos/stages/battle.gd
@@ -4,7 +4,7 @@ var positions = [Vector2(200, 300), Vector2(800, 300), Vector2(510, 100), Vector
 
 var char_scn = preload("res://characters/character.tscn")
 
-var living = []
+var living = 0
 
 func _ready():
 	# gets the variables set in the CSS
@@ -14,21 +14,35 @@ func _ready():
 	# which ports are living
 	for key in a_d.keys():
 		var player = a_d[key]
-		living.append(player)
+		living += 1
 		
 		var character = s_c[player - 1]
-		print(character)
 		var sprite_frames = load(str("res://characters/sprites/",character,".tres"))
 		
-		# creates a character and sets its skin and position
+		# creates a character and sets its skin, position and other variables
 		var char_inst = char_scn.instance()
 		char_inst.set_name(str("Character", player))
 		char_inst.get_node("sprite").set_sprite_frames(sprite_frames)
+		char_inst.connect("death", self, "anotherOneBitesTheDust")
+		char_inst.character_name = character
 		char_inst.position = positions[player - 1]
 		char_inst.controller_device = key
 		
 		add_child(char_inst)
 	set_process(true)
 
+
 func _process(delta):
-	pass
+	check_end_condition()
+
+
+func anotherOneBitesTheDust():
+	living -= 1
+
+
+func check_end_condition():
+	if living <= 1:
+		set_process(false)
+		for node in get_children():
+			if node.is_in_group("Player"):
+				print(str(node.character_name, " wins!!"))
