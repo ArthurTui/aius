c2b6402b38f5d23105ffe928c177603a21849fb7
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-05-10 15:10:06 -0300

    fix nature2 spell

--- maguinhos/spells/nature/nature2.gd
+++ maguinhos/spells/nature/nature2.gd
@@ -3,65 +3,70 @@ extends "res://spells/base_spell.gd"
 const SPEED = 6
 const DAMAGE = 15
 const KNOCKBACK = 5
-var element = 1 # Lightning = 0, Nature = 1, Fire = 2, Water = 3
-var level = 2
 
-var direction = Vector2( 0, 0 ) # direction that the seed flies to
-var parent
+const SEED_ROOT = .5
+const THORN_ROOT = 1.5
+const THORN_RADIUS = 30
+
+var direction = Vector2() # direction that the seed flies to
 var is_seed = true
 
-
-#func _ready():
-#	get_node( "SFX" ).play( "leaf" )
-
-
-func fire( direction, parent ):
+func fire(direction, caster):
 	self.direction = direction
-	self.parent = parent
-	set_rotation( direction.angle() - deg2rad(90) )
-	set_position( parent.position )
-	set_process( true )
+	self.caster = caster
+	rotation = direction.angle()
+	set_position(caster.position)
+	set_process(true)
 
 
 func _process(delta):
 	position += direction * SPEED
 
 
-func _on_Area2D_body_enter( body ):
-	if body != parent:
-		grow()
+func _on_Area2D_body_enter(body):
+	if body != caster:
 		$GrowTimer.stop()
-		if body.has_method( "take_damage" ):
-			# If the thorns are grown, play death animation
-			# when enemy enters them. Otherwise, the seed just
-			# disappears dealing damage
+		if body.has_method("take_damage"):
+			# If the thorns are grown, play death animation when enemy enters
+			# them. Otherwise, the seed just disappears dealing damage
 			if !is_seed:
 				body.take_damage(2 * DAMAGE, null)
-				body.root(1)
-				_on_LifeTimer_timeout()
-			else:
-				var kb_direction = (body.position - position).normalized()
-				body.take_damage(DAMAGE, kb_direction, KNOCKBACK)
+				body.root(THORN_ROOT)
 				die()
+			else:
+#				var kb_direction = (body.position - position).normalized()
+				body.take_damage(DAMAGE, null)
+				body.root(SEED_ROOT)
+				die()
+		else:
+			grow()
 
 
 func _on_GrowTimer_timeout():
-	$LifeTimer.start()
-	is_seed = false
 	grow()
 
 
 # Projectile stops moving and expands
 func grow():
-	set_process( false )
-	$AnimationPlayer.play( "grow" )
-	$Area2D.set_scale(Vector2(2.5, 2.5))
+	if !is_seed:
+		return
+	set_process(false)
+	$LifeTimer.start()
+	is_seed = false
+	rotation = 0
+	$AnimatedSprite.play("grow")
+	$Projectile/CollisionShape2D.shape.radius = THORN_RADIUS
 
 
 func _on_LifeTimer_timeout():
-	$AnimationPlayer.play( "die" )
+	die()
 
 
 func die():
-	$Area2D.queue_free()
+	if !has_node("Projectile"):
+		return
+	$Projectile.queue_free()
+	if !is_seed:
+		$AnimatedSprite.play("die")
+		yield($AnimatedSprite, "animation_finished")
 	queue_free()
