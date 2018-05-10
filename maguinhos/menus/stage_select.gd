e8f9e498e74d0fec23b3def9256d6d1aeb51b8e2
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-10 17:20:27 -0300

    Progresso na stage select screen Suporte para controle para mover cursor Thumbnails temporarias eks dee CSS passa para tela de seleção de fase ao inves de direto para forest
    Alguns bugs identificados, mas ainda não consertados (ver issues)

--- maguinhos/menus/stage_select.gd
+++ maguinhos/menus/stage_select.gd
@@ -1,22 +1,44 @@
 extends Control
 
+const stage_names = ["forest", "river", "city", "cave"]
+
 
 func _ready():
 	pass
 
 
 func _process(delta):
+	# left controller axis
+	var h = Input.get_joy_axis(0, 0)/2 # horizontal
+	var v = Input.get_joy_axis(0, 1)/2 # vertical
 	var direction = Vector2(0, 0)
 	
-	if Input.is_action_pressed(str("ui_left")):
-	    direction -= Vector2( 1, 0 )
-	if Input.is_action_pressed(str("ui_right")):
-	    direction += Vector2( 1, 0 )
-	if Input.is_action_pressed(str("ui_down")):
-	    direction += Vector2( 0, 1 )
-	if Input.is_action_pressed(str("ui_up")):
+	
+	if Input.is_action_pressed("ui_up"):
 	    direction -= Vector2( 0, 1 )
+	if Input.is_action_pressed("ui_right"):
+	    direction += Vector2( 1, 0 )
+	if Input.is_action_pressed("ui_down"):
+	    direction += Vector2( 0, 1 )
+	if Input.is_action_pressed("ui_left"):
+	    direction -= Vector2( 1, 0 )
+	
+	
+	direction.x += h
+	direction.y += v
+	
+	if direction.x <= 0.05 and direction.x >= -0.05:
+		direction.x = 0
+	if direction.y <= 0.05 and direction.y >= -0.05:
+		direction.y = 0
 		
-	var pos = $cursor.get_position() + (direction.normalized())
-	print (pos)
-	$cursor.set_position(pos)
+	$cursor.position += direction * 10
+	var cursor_pos = $cursor.get_position()
+	
+	for node in $stages.get_children():
+		if node.name in stage_names and node.get_child(0).overlaps_body($cursor):
+			if Input.is_action_just_pressed("ui_accept"):
+				if node.name == "forest":
+					get_tree().change_scene("res://stages/forest.tscn")
+				else:
+					print(node.name)
