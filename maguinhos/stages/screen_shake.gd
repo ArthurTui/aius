2183d1897f4020adc40b813958b8e6e0e0be2626
ArthurTui <arthur.vb@gmail.com>
2018-05-11 09:18:25 -0300

    tweaks to screen shake documentation

--- maguinhos/stages/screen_shake.gd
+++ maguinhos/stages/screen_shake.gd
@@ -9,15 +9,18 @@ const MAX_OFFSET_X = 20
 const MAX_OFFSET_Y = 20
 
 # The ratio at which the screen shake decreases. It is multiplied by dt in
-# process, so screen_shake goes from 1 to 0 in 1 / DEC_RATIO seconds.
+# process, so screen_shake goes from 1 to 0 in (1 / DEC_RATIO) seconds.
 const DEC_RATIO = 1.5
 
 # The expoent when factoring screen shake into the actual position and rotation
 # offsets. Suggested value is between 2 and 3.
 const EXP = 2
 
-# A value representing current screen shake factor ranging from 0 to 1.
+# A value representing current screen shake ranging from [0, 1].
 var screen_shake = 0
+
+# The actual range (also [0, 1]) that the positional and rotational offsets can
+# be multiplied by.
 var shake_factor = 0
 
 func _ready():
@@ -40,9 +43,8 @@ func _process(delta):
 		get_parent().get_node("HelpHUD/VBox/ShakeBar").value = screen_shake
 		get_parent().get_node("HelpHUD/VBox/FactorBar").value = shake_factor
 	
-	var x = rand_range(-1, 1) * shake_factor * MAX_OFFSET_X
-	var y = rand_range(-1, 1) * shake_factor * MAX_OFFSET_Y
-	position = Vector2(x, y)
+	position.x = rand_range(-1, 1) * shake_factor * MAX_OFFSET_X
+	position.y = rand_range(-1, 1) * shake_factor * MAX_OFFSET_Y
 	rotation_degrees = rand_range(-1, 1) * shake_factor * MAX_ANGLE
 	
 	screen_shake = max(0, screen_shake - DEC_RATIO * delta)
