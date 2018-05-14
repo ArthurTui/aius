44d4e28b0c4cc68c35448e0209e6502f8d20bcaa
cerco <henriquecerquinho@gmail.com>
2018-05-14 11:15:05 -0300

    botão de random na stage select e algumas inclusões estéticas não terminadas

--- maguinhos/menus/stage_select.gd
+++ maguinhos/menus/stage_select.gd
@@ -1,10 +1,12 @@
 extends Control
 
-const stage_names = ["forest", "river", "city", "cave"]
+
+const stage_names = ["forest", "river", "city", "cave", "random"]
+var stages_hovered = [false, false, false, false]
 
 
 func _ready():
-	pass
+	randomize()
 
 
 func _process(delta):
@@ -36,9 +38,46 @@ func _process(delta):
 	var cursor_pos = $cursor.get_position()
 	
 	for node in $stages.get_children():
-		if node.name in stage_names and node.get_child(0).overlaps_body($cursor):
-			if Input.is_action_just_pressed("ui_accept"):
-				if node.name == "forest":
-					get_tree().change_scene("res://stages/forest.tscn")
-				else:
-					print(node.name)
+		var name = node.name
+		
+		if name in stage_names:
+			var idx = stage_names.find(name)
+			
+			if node.get_child(0).overlaps_body($cursor):
+				
+				# shows stage info
+				if idx < 4 and not stages_hovered[idx]:
+					stages_hovered[idx] = true
+					$stages.get_node(str(name,"/info/anim")).play("enter")
+				
+				# changes random's color if hovered
+				if name == "random":
+					$stages/random.set_frame_color("7ef55c") # green
+				
+				# stage was selected
+				if Input.is_action_just_pressed("ui_accept"):
+					if name != "random":
+						# we'll use this once all stages are instanceable
+						#get_tree().change_scene(str("res://stages/",name,".tscn"))
+						
+						print(name)
+						if name == "forest":
+							get_tree().change_scene("res://stages/forest.tscn")
+						
+					# random stage, also teleports the cursor to the stage
+					# the random chose
+					elif name == "random":
+						var stage = stage_names[randi() % 4]
+						var pos = $stages.get_node(stage).get_position()
+						pos += $stages.get_node(stage).get_pivot_offset()
+						$cursor.position = pos
+						
+			# resets random's color
+			if not $stages/random/Area2D.overlaps_body($cursor):
+				$stages/random.set_frame_color("7ad6ef") # blue
+		
+			# hides stage info
+			elif idx < 4 and stages_hovered[idx]:
+				stages_hovered[idx] = false
+				$stages.get_node(str(name,"info/anim")).play("exit")
+				
