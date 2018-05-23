be1db354451367401f6f40c6fce3ae7fd55d60db
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-23 18:50:55 -0300

    forest agora inicializa personagens nas posiçoes certas e com rainbow label na posição certa

--- maguinhos/stages/forest.gd
+++ maguinhos/stages/forest.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var positions = [Vector2(200, 300), Vector2(800, 300), Vector2(510, 100), Vector2(510, 500)]
+var positions = [Vector2(480, 480), Vector2(960, 240), Vector2(1440, 480), Vector2(960, 720)]
 
 var char_scn = preload("res://characters/character.tscn")
 var testing = true
@@ -28,7 +28,7 @@ func _ready():
 		var char_inst = char_scn.instance()
 		char_inst.set_name(str("Character", player))
 		char_inst.get_node("sprite").set_sprite_frames(sprite_frames)
-		char_inst.connect("death", self, "anotherOneBitesTheDust")
+		char_inst.connect("death", self, "another_one_bites_the_dust")
 		char_inst.character_name = character
 		char_inst.position = positions[player - 1]
 		char_inst.controller_device = key
@@ -45,7 +45,7 @@ func _process(delta):
 	check_end_condition()
 
 
-func anotherOneBitesTheDust():
+func another_one_bites_the_dust():
 	living -= 1
 
 
