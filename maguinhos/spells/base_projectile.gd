0c01e63542a22e66dc17eade6c70a9ea47abb53c
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-23 18:50:12 -0300

    spells precisam colidir com hitbox do personagem

--- maguinhos/spells/base_projectile.gd
+++ maguinhos/spells/base_projectile.gd
@@ -12,14 +12,15 @@ func _on_projectile_area_entered(area):
 	var other = area.get_parent()
 	
 	# Handle exceptions
-	if area.get_name() == "detection_area":
+	name = area.get_name()
+	if name == "detection_area":
 		return
 	if other == self:
 		return
 	if "caster" in other and other.caster == self.caster:
 		return
 	
-	if "element" in area: # Makes sure it's something interactable with projectile
+	if "element" in area or name == "hitbox": # Makes sure it's something interactable with projectile
 		if area.level > self.level:
 			get_parent().die()
 		elif area.element == (self.element + 1) % 4: # Oposing element
