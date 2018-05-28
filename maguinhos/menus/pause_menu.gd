90233c9d04fc1b35603e3238131aed1d3e20f641
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-28 13:48:25 -0300

    pause screen decente

--- maguinhos/menus/pause_menu.gd
+++ maguinhos/menus/pause_menu.gd
@@ -1,27 +1,41 @@
 extends Panel
 
 var can_process = false
+var hovered = 0
 
 func _ready():
 	self.visible = false
 	get_tree().paused = false
 
 
-func _process(delta):
-	# for some reason, the "start" a player pressed on the CSS
-	# was registered here and paused the game everytime the
-	# battle scene was instanciated, so the "can_process" variable
-	# and the work timer are a workaround
+func _input(event):
+	print("resume",$resume.rect_position)
+	print($quit.rect_position)
+	print($rect.rect_position)
 	if Input.is_action_just_pressed("pause") and can_process:
-		_on_resume_pressed()
+		on_resume_pressed()
+	if Input.is_action_just_pressed("ui_up"):
+		$rect.set_position($resume.rect_position)
+		hovered = 0
+	elif Input.is_action_just_pressed("ui_down"):
+		$rect.set_position($quit.rect_position)
+		hovered = 1
+		
+	elif Input.is_action_just_pressed("ui_accept") and self.visible:
+		match hovered:
+			0:
+				on_resume_pressed()
+			1:
+				on_quit_pressed()
+	
 
 
-func _on_resume_pressed():
+func on_resume_pressed():
 	get_tree().paused = not get_tree().is_paused()
 	self.visible = not self.is_visible()
 	
 
-func _on_quit_pressed():
+func on_quit_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://menus/CSS.tscn")
 
